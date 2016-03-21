09b8c285b7004cd38c68b21d268e415cbe3fbd91
MarvellousChester <PhantomX16@mail.ru>
2016-03-21 16:41:54 +0200

    Added new additional cost line to customers account; Added additional cost to system configuration
    TODO:  email

---
+++ app/code/local/Cgi/AddShippingCost/Block/Order/Totals.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * AddShippingCost Sales Order Totals rewrite block
+ * Add new Additional Shipping Cost to customer account front-end
+ *
+ * @category   Cgi
+ * @package    AddShippingCost
+ * @author     Bobok Aleksandr CGI Trainee Group
+ *
+ */
+class Cgi_AddShippingCost_Block_Order_Totals extends Mage_Sales_Block_Order_Totals
+{
+    /**
+     * Initialize order totals array
+     *
+     * @return Mage_Sales_Block_Order_Totals
+     */
+    protected function _initTotals()
+    {
+        parent::_initTotals();
+        $amount = $this->getSource()->getAddCost();
+        $createHelper = Mage::helper('addshippingcost/varienObjectCreate');
+        if ($amount) {
+            $this->addTotalBefore($createHelper->create($amount));
+        }
+
+        return $this;
+    }
+
+}
